14a7ae82e0fd86800cdd21597ad38997eae172e5
AnkitSingh60 <ankitanwar730@gmail.com>
2022-07-22 11:15:59 +0530

    functionality added

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tags = () => {
+    const [tags, setTags] = useState([])
+
+    const handleKeyDown = (event) => {
+        if(event.key !== "Enter") return;
+
+        const tagInput = event.target.value;
+
+        if(!tagInput) return;
+
+        setTags([...tags, tagInput]);
+
+        event.target.value = ""
+
+        
+    }
+
   return (
     <>
     <div className="container">
-        <div className="tags">
-            <span className="input-text">Ankit Singh</span>
+       {
+        tags.map((tag) => (
+            <div className="tags">
+            <span className="input-text">{tag}</span>
             <span className="close">
             x
             </span>
         </div>
-        <div className="tags">
-            <span className="input-text">heyy kiddo</span>
-            <span className="close">
-            x
-            </span>
-        </div>
-
+        ))
+       }
+        <input onKeyDown={handleKeyDown} className="addInput" type="text" placeholder="add a tag..." />
         </div>
         <p className="label_p">Hit 'Enter' to add a topic</p>
     </>
